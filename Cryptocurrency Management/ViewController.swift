d08c62924ade0c4abc78dd269065625298aca43a
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-01-19 08:58:41 +0900

    setup

--- Cryptocurrency Management/ViewController.swift
+++ Cryptocurrency Management/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let addCurrencyButton: UIButton = UIButton()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(hex: "#010A43")
+        createAddCurrencyButton()
     }
 
+    
+    func createAddCurrencyButton() {
+        addCurrencyButton.setTitle("Add Currency", for: .normal)
+        addCurrencyButton.setTitleColor(UIColor(hex: "#426DDC"), for: .normal)
+        addCurrencyButton.titleLabel?.font = UIFont(name: "HelveticaNeueCyr", size: 14)
+        addCurrencyButton.frame = CGRect(x: 261, y: 76, width: 120, height: 36)
+        addCurrencyButton.backgroundColor = UIColor(hex: "#212A6B", alpha: 1.0)
+        addCurrencyButton.addTarget(self, action: #selector(self.addCurrencyButtonTapped(_:)), for: .touchUpInside)
+
+        self.view.addSubview(addCurrencyButton)
+    }
+    
+    @objc func addCurrencyButtonTapped(_ sender: UIButton) {
+        let nextView = AddCurrencyViewController()
+        nextView.modalTransitionStyle = .coverVertical
+        self.present(nextView, animated: true, completion: nil)
+
+    }
 
 }
 
