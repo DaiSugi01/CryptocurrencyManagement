feead8e1b3137def6355397e626a43be251d0e3c
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-01-19 13:49:02 +0900

    addCurrencyButton IIFE

--- Cryptocurrency Management/ViewController.swift
+++ Cryptocurrency Management/ViewController.swift
@@ -8,41 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let addCurrencyButton: UIButton = UIButton()
-//    var addCurrencyButton: UIButton = UIButton()
+//    let addCurrencyButton: UIButton = UIButton()
+    let addCurrencyButton: UIButton = {
+        let bt = UIButton()
+        bt.translatesAutoresizingMaskIntoConstraints = false
+        bt.setTitle("Add Currency", for: .normal)
+        bt.setTitleColor(UIColor(hex: "#426DDC"), for: .normal)
+        bt.titleLabel?.font = UIFont(name: "HelveticaNeueCyr", size: 14)
+        bt.frame = CGRect(x: 261, y: 76, width: 120, height: 36)
+        bt.backgroundColor = UIColor(hex: "#212A6B", alpha: 1.0)
+        bt.addTarget(self, action: #selector(addCurrencyButtonTapped(_:)), for: .touchUpInside)
+        return bt
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         view.backgroundColor = UIColor(hex: "#010A43")
-        createAddCurrencyButton()
+        view.addSubview(addCurrencyButton)
     }
 
     
-    func createAddCurrencyButton() {
-//        addCurrencyButton = {
-//            let bt = UIButton()
-//            bt.translatesAutoresizingMaskIntoConstraints = false
-//            bt.setTitleColor(UIColor(hex: "#426DDC"), for: .normal)
-//            bt.titleLabel?.font = UIFont(name: "HelveticaNeueCyr", size: 14)
-//            bt.frame = CGRect(x: 261, y: 76, width: 120, height: 36)
-//            bt.backgroundColor = UIColor(hex: "#212A6B", alpha: 1.0)
-//            bt.addTarget(self, action: #selector(self.addCurrencyButtonTapped(_:)), for: .touchUpInside)
-//            bt.addTarget(self, action: #selector(self.addCurrencyButtonTapped(_:)), for: .touchUpInside)
-//            self.view.addSubview(bt)
-//            return bt
-//        }()
-        
-        
-        addCurrencyButton.setTitle("Add Currency", for: .normal)
-        addCurrencyButton.setTitleColor(UIColor(hex: "#426DDC"), for: .normal)
-        addCurrencyButton.titleLabel?.font = UIFont(name: "HelveticaNeueCyr", size: 14)
-        addCurrencyButton.frame = CGRect(x: 261, y: 76, width: 120, height: 36)
-        addCurrencyButton.backgroundColor = UIColor(hex: "#212A6B", alpha: 1.0)
-        addCurrencyButton.addTarget(self, action: #selector(self.addCurrencyButtonTapped(_:)), for: .touchUpInside)
+//    func createAddCurrencyButton() {
+//
+//        addCurrencyButton.setTitle("Add Currency", for: .normal)
+//        addCurrencyButton.setTitleColor(UIColor(hex: "#426DDC"), for: .normal)
+//        addCurrencyButton.titleLabel?.font = UIFont(name: "HelveticaNeueCyr", size: 14)
+//        addCurrencyButton.frame = CGRect(x: 261, y: 76, width: 120, height: 36)
+//        addCurrencyButton.backgroundColor = UIColor(hex: "#212A6B", alpha: 1.0)
+//        addCurrencyButton.addTarget(self, action: #selector(self.addCurrencyButtonTapped(_:)), for: .touchUpInside)
 
-        self.view.addSubview(addCurrencyButton)
-    }
+//        self.view.addSubview(addCurrencyButton)
+//    }
     
     @objc func addCurrencyButtonTapped(_ sender: UIButton) {
         let nextView = AddCurrencyViewController()
