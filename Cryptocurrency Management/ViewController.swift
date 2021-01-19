6e7e1a22edc47ef5c4340e25ca3e55ab53b59f72
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-01-19 09:54:33 +0900

    delete self

--- Cryptocurrency Management/ViewController.swift
+++ Cryptocurrency Management/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     let addCurrencyButton: UIButton = UIButton()
-    
+//    var addCurrencyButton: UIButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,20 @@ class ViewController: UIViewController {
 
     
     func createAddCurrencyButton() {
+//        addCurrencyButton = {
+//            let bt = UIButton()
+//            bt.translatesAutoresizingMaskIntoConstraints = false
+//            bt.setTitleColor(UIColor(hex: "#426DDC"), for: .normal)
+//            bt.titleLabel?.font = UIFont(name: "HelveticaNeueCyr", size: 14)
+//            bt.frame = CGRect(x: 261, y: 76, width: 120, height: 36)
+//            bt.backgroundColor = UIColor(hex: "#212A6B", alpha: 1.0)
+//            bt.addTarget(self, action: #selector(self.addCurrencyButtonTapped(_:)), for: .touchUpInside)
+//            bt.addTarget(self, action: #selector(self.addCurrencyButtonTapped(_:)), for: .touchUpInside)
+//            self.view.addSubview(bt)
+//            return bt
+//        }()
+        
+        
         addCurrencyButton.setTitle("Add Currency", for: .normal)
         addCurrencyButton.setTitleColor(UIColor(hex: "#426DDC"), for: .normal)
         addCurrencyButton.titleLabel?.font = UIFont(name: "HelveticaNeueCyr", size: 14)
@@ -33,7 +47,7 @@ class ViewController: UIViewController {
     @objc func addCurrencyButtonTapped(_ sender: UIButton) {
         let nextView = AddCurrencyViewController()
         nextView.modalTransitionStyle = .coverVertical
-        self.present(nextView, animated: true, completion: nil)
+        present(nextView, animated: true, completion: nil)
 
     }
 
