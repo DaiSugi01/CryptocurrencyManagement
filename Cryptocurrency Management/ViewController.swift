71e233f12b615dff3b87aa66171dccc9ebf701a8
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-01-21 08:01:05 +0900

    modified table view

--- Cryptocurrency Management/ViewController.swift
+++ Cryptocurrency Management/ViewController.swift
@@ -4,12 +4,21 @@
 //
 //  Created by 杉原大貴 on 2021/01/17.
 //
-
 import UIKit
+private enum State {
+    case closed
+    case open
+}
+extension State {
+    var opposite: State {
+        switch self {
+        case .open: return .closed
+        case .closed: return .open
+        }
+    }
+}
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    let headerNavigationBar: UIView = UIView()
-    let chartView: UIView = UIView()
+class ViewController: UIViewController {
     let addCurrencyButton: UIButton = {
         let bt = UIButton()
         bt.translatesAutoresizingMaskIntoConstraints = false
@@ -21,158 +30,113 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         bt.addTarget(self, action: #selector(addCurrencyButtonTapped(_:)), for: .touchUpInside)
         return bt
     }()
-    let showChartButton: UIButton = {
-        let bt = UIButton()
-        bt.translatesAutoresizingMaskIntoConstraints = false
-
-
-        bt.frame = CGRect(x: 30, y: 76, width: 40, height: 40)
-        bt.backgroundColor = UIColor(hex: "#212A6B", alpha: 1.0)
-        bt.layer.cornerRadius = 15.0
-        bt.addTarget(self, action: #selector(showChartButtonTapped(_:)), for: .touchUpInside)
-        return bt
+    let cellId = "currencyCellId"
+    private var currentState: State = .closed
+    private lazy var popupView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(hex: "010A43")
+        view.layer.cornerRadius = 20
+        return view
     }()
-    var helloLabel: UILabel = {
-        let lb = UILabel()
-        lb.translatesAutoresizingMaskIntoConstraints = false
-        lb.frame = CGRect(x: 60, y: 76, width: 200, height: 36)
-        lb.textAlignment = NSTextAlignment.center
-        lb.text = "Hello Daiki,"
-        lb.backgroundColor = UIColor(hex: "#010A43")
-        lb.textColor = .white
-        return lb
+    let currencyTableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.backgroundColor = .gray
+        return tv
     }()
-    let tableViewHeader: UIView = UIView()
-    let tableView: UITableView = UITableView()
-    var selectedCurrencies: [Cryptocurrency] =
-        [Cryptocurrency(name: "Bitcoin", price: 45497.94),
-         Cryptocurrency(name: "Ethereum", price: 1408.84),
-         Cryptocurrency(name: "Ripple", price: 0.301),
-         Cryptocurrency(name: "Litecoin", price: 180.64)]
-    var showChartButtonTappedState = false
-
-
-
+    
     override func viewDidLoad() {
+        view.backgroundColor = .white
         super.viewDidLoad()
-
-
-        view.backgroundColor = UIColor(hex: "#010A43")
-        headerNavigationBarFunc()
-        charViewFunc()
-        setupTableView()
-
-        headerNavigationBar.addSubview(showChartButton)
-        headerNavigationBar.addSubview(helloLabel)
-        headerNavigationBar.addSubview(addCurrencyButton)
+//        view.backgroundColor = UIColor(hex: "#010A43")
+        view.addSubview(addCurrencyButton)
         NSLayoutConstraint.activate([
-            addCurrencyButton.topAnchor.constraint(greaterThanOrEqualTo: headerNavigationBar.safeAreaLayoutGuide.topAnchor, constant: 50),
-            addCurrencyButton.trailingAnchor.constraint(greaterThanOrEqualTo: headerNavigationBar.safeAreaLayoutGuide.trailingAnchor, constant: -23),
-            showChartButton.topAnchor.constraint(greaterThanOrEqualTo: headerNavigationBar.safeAreaLayoutGuide.topAnchor, constant: 50),
-            showChartButton.leadingAnchor.constraint(greaterThanOrEqualTo: headerNavigationBar.safeAreaLayoutGuide.leadingAnchor, constant: 30),
-            helloLabel.topAnchor.constraint(greaterThanOrEqualTo: headerNavigationBar.safeAreaLayoutGuide.topAnchor, constant: 55),
-            helloLabel.leadingAnchor.constraint(greaterThanOrEqualTo: showChartButton.safeAreaLayoutGuide.trailingAnchor, constant: 20),
-            chartView.topAnchor.constraint(equalTo: headerNavigationBar.bottomAnchor, constant: 10),
-            chartView.leadingAnchor.constraint(equalTo: headerNavigationBar.leadingAnchor, constant: 10)
-//            chartView.widthAnchor.constraint(equalTo: headerNavigationBar.widthAnchor, multiplier: 0.9),
-//            chartView.heightAnchor.constraint(equalToConstant: 200)
-
-
+            addCurrencyButton.topAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            addCurrencyButton.trailingAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -23)
         ])
+        currencyTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        currencyTableView.delegate = self
+        currencyTableView.dataSource = self
+        layout()
     }
 
-
+    
     @objc func addCurrencyButtonTapped(_ sender: UIButton) {
         let nextView = AddCurrencyViewController()
         nextView.modalTransitionStyle = .coverVertical
         present(nextView, animated: true, completion: nil)
-
+    }
+    
+    private var bottomConstraint = NSLayoutConstraint()
+    private func layout() {
+        view.backgroundColor = UIColor(hex: "10194E")
+        popupView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(popupView)
+        popupView.addSubview(currencyTableView)
+        
+        popupView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 2).isActive = true
+        popupView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -2).isActive = true
+        bottomConstraint = popupView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: view.frame.size.height * 0.3)
+        bottomConstraint.isActive = true
+        popupView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.9).isActive = true
+        
+        currencyTableView.heightAnchor.constraint(equalTo: popupView.heightAnchor, multiplier: 0.9).isActive = true
+        currencyTableView.bottomAnchor.constraint(equalTo: popupView.bottomAnchor).isActive = true
+        currencyTableView.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        currencyTableView.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
     }
 
-    @objc func showChartButtonTapped(_ sender: UIButton) {
-        let width = self.view.frame.width
-        if showChartButtonTappedState == false {
-            showChartButtonTappedState = true
-        } else {
-            showChartButtonTappedState = false
-        }
-        UIView.animate(withDuration: 1.0, delay: 0.2, usingSpringWithDamping: 0.7, initialSpringVelocity: 10) {
-            if self.showChartButtonTappedState == true {
-                self.headerNavigationBar.frame = CGRect(x: 0, y: 0, width: width, height: 358)
-                let height = self.headerNavigationBar.frame.height
-                self.chartView.frame = CGRect(x: 10, y: height - self.chartView.frame.height - 10, width: width - 20, height: 180)
-                self.chartView.isHidden = false
-                self.tableView.frame = CGRect(x: self.tableView.frame.origin.x, y: self.tableView.frame.origin.y + 200, width: self.tableView.frame.size.width, height: self.tableView.contentSize.height)
-            } else {
-                self.headerNavigationBar.frame = CGRect(x: 0, y: 0, width: width, height: 158)
-                let height = self.headerNavigationBar.frame.height
-                self.chartView.frame = CGRect(x: 10, y: height - self.chartView.frame.height - 10, width: width - 20, height: 180)
-                self.chartView.isHidden = true
-                self.tableView.frame = CGRect(x: self.tableView.frame.origin.x, y: self.tableView.frame.origin.y - 200, width: self.tableView.frame.size.width, height: self.tableView.contentSize.height)
-            }
-        }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-    func headerNavigationBarFunc() {
-        let width = self.view.frame.width
-        let originalFrame = CGRect(x: 0, y: 0, width: width, height: 160)
-
-        headerNavigationBar.backgroundColor = UIColor(hex: "#010A43")  // 010A43 DDDDDD
-        headerNavigationBar.frame = originalFrame
-
-        self.view.addSubview(headerNavigationBar)
-    }
-
-    func charViewFunc() {
-        let width = self.view.frame.width
-        chartView.backgroundColor = .green
-        chartView.frame = CGRect(x: 10, y: 170, width: width - 20, height: 180)
-        self.headerNavigationBar.addSubview(chartView)
-        chartView.isHidden = true
-    }
-
-
-    func setupTableView() {
-        // required
-        tableView.dataSource = self
-        tableView.delegate = self
-
-        view.addSubview(tableView)
-
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.topAnchor.constraint(equalTo: headerNavigationBar.bottomAnchor, constant: 80).isActive = true
-        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return selectedCurrencies.count
-    }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.backgroundColor = UIColor(hex: "#10194E")
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath)
-            // ?? UITableViewCell(style: .value1, reuseIdentifier: "cell")  // UITableViewCell.CellStyle.value1
-        // if that fails, create a new one
-//        if cell == nil {
-//            cell = UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: "cell")
-//        }
-
-        cell.textLabel?.text = selectedCurrencies[indexPath.row].name  // 858EC5
-        cell.textLabel?.textColor = UIColor(hex: "#858EC5")
-        cell.detailTextLabel?.trailingAnchor.constraint(equalTo: tableView.trailingAnchor, constant: -20).isActive = true
-        cell.detailTextLabel?.text = "\(selectedCurrencies[indexPath.row].price)"
-        if indexPath.row % 2 == 0 {
-            cell.backgroundColor = UIColor(hex: "#192259")
-        } else {
-            cell.backgroundColor = UIColor(hex: "#10194E")
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = indexPath.row % 2 == 0 ? UIColor(hex: "192259") : UIColor(hex: "10194E")
+        cell.textLabel?.textColor = UIColor(hex: "858EC5")
+        cell.textLabel?.text = "TEST Label"
+        cell.textLabel?.font = .boldSystemFont(ofSize: 17)
         return cell
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let state = currentState.opposite
+        let transitionAnimator = UIViewPropertyAnimator(duration: 1, dampingRatio: 1, animations: {
+            switch state {
+            case .open:
+                self.bottomConstraint.constant = self.view.frame.height * 0.9
+            case .closed:
+                self.bottomConstraint.constant = self.view.frame.size.height * 0.4
+            }
+            self.view.layoutIfNeeded()
+        })
+        transitionAnimator.addCompletion { position in
+            switch position {
+            case .start:
+                self.currentState = state.opposite
+            case .end:
+                self.currentState = state
+            case .current:
+                ()
+            }
+            switch self.currentState {
+            case .open:
+                self.bottomConstraint.constant = self.view.frame.height * 0.9
+            case .closed:
+                self.bottomConstraint.constant = self.view.frame.size.height * 0.4
+            }
+        }
+        transitionAnimator.startAnimation()
+    }
 }
