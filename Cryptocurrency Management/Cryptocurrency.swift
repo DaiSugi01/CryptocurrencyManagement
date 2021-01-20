e95be457e40fcfc2d8027335927b9c770cc5aeec
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-01-21 07:20:58 +0900

    pull master then rewrite ViewController

---
+++ Cryptocurrency Management/Cryptocurrency.swift
@@ -0,0 +1,13 @@
+//
+//  Cryptocurrency.swift
+//  Cryptocurrency Management
+//
+//  Created by Yuki Tsukada on 2021/01/21.
+//
+
+import Foundation
+
+struct Cryptocurrency: Codable {
+    var name: String
+    var price: Double
+}
